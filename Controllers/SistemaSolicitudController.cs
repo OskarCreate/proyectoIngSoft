7ec987e06e877324f307433b96534c54d33dcb8e
DaysGone <kevinpasionpayano@gmail.com>
2025-09-26 21:40:13 -0500

    cambiando

--- Controllers/SistemaSolicitudController.cs
+++ Controllers/SistemaSolicitudController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,9 +18,11 @@ namespace proyectoIngSoft.Controllers
         {
             _logger = logger;
         }
+        
 
         public IActionResult Index()
         {
+
             return View();
         }
 
