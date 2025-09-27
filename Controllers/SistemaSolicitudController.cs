47479292a864180da2a1dcc64be8402c3eaf7fec
Yitzak Zamudio <codemanhh78@gmail.com>
2025-09-27 02:58:40 -0500

    Commit hu09

--- Controllers/SistemaSolicitudController.cs
+++ Controllers/SistemaSolicitudController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using proyectoIngSoft.Data;
+using proyectoIngSoft.Models;
 
 namespace proyectoIngSoft.Controllers
 {
@@ -13,10 +15,13 @@ namespace proyectoIngSoft.Controllers
     public class SistemaSolicitudController : Controller
     {
         private readonly ILogger<SistemaSolicitudController> _logger;
+        
+        private readonly ApplicationDbContext _context;
 
-        public SistemaSolicitudController(ILogger<SistemaSolicitudController> logger)
+        public SistemaSolicitudController(ILogger<SistemaSolicitudController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
         
 
@@ -24,6 +29,61 @@ namespace proyectoIngSoft.Controllers
         {
 
             return View();
+        }
+
+         public IActionResult ListaSolicitudes()
+        {
+            var lista = _context.DbSetDescanso
+                .Include(d => d.User)
+                .Include(d => d.TipoDescanso)
+                .Select(d => new Lista
+                {
+                    Username = d.User.Username,
+                    Apellidos = d.User.Apellidos,
+                    Dni = d.User.Dni,
+                    Observaciones = d.TipoDescanso.Nombre,
+                    FechaSolicitud = d.FechaSolicitud,
+                    Estado = "En Proceso", // Puedes mapear según tu lógica
+                    IdUser = d.User.IdUser,
+                    IdDescanso = d.IdDescanso
+                })
+                .ToList();
+
+            return View("ListaSolicitudes", lista);
+ 
+        }
+
+        public IActionResult DetalleDescanso(int descansoId)
+        {
+            var descanso = _context.DbSetDescanso
+                .Include(d => d.User)
+                .Include(d => d.TipoDescanso)
+                .Include(d => d.Accidente)
+                .Include(d => d.Enfermedad)
+                .Include(d => d.EnfermedadFam)
+                .Include(d => d.Fallecimiento)
+                .Include(d => d.Maternidad)
+                .Include(d => d.Paternidad)
+                .Include(d => d.DocumentosMedicos)
+                .FirstOrDefault(d => d.IdDescanso == descansoId);
+
+            if (descanso == null) return NotFound();
+
+            return PartialView("_DetalleDescanso", descanso);
+        }
+
+        public IActionResult VerDocumento(int id)
+        {
+            var documento = _context.DocumentosMedicos
+                .AsNoTracking()
+                .FirstOrDefault(d => d.IdDocumento == id);
+
+            if (documento == null || documento.Archivo == null || documento.Archivo.Length == 0)
+                return NotFound();
+
+            // Forzar inline para que se vea en el iframe
+            Response.Headers["Content-Disposition"] = $"inline; filename=\"{documento.Nombre}\"";
+            return File(documento.Archivo, "application/pdf");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
