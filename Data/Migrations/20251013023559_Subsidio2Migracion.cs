a2eadb93190653e3542ed46dc17ee2f38203d2ef
junior_tovar <junior_tovar@usmp.pe>
2025-10-18 10:20:30 -0500

    Primer commit de la rama Hu25correc

---
+++ Data/Migrations/20251013023559_Subsidio2Migracion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyectoIngSoft.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Subsidio2Migracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EstadoSubsidioJ",
+                table: "t_Descanso",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstadoSubsidioJ",
+                table: "t_Descanso");
+        }
+    }
+}
